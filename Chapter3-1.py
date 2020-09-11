5387c1410ce7cea11ccf3d6440a4927c94e591d1
dperkheezy <dperkins1122@gmail.com>
2020-09-10 21:26:59 -0700

    Making and Calling Lists

---
+++ Chapter3-1.py
@@ -0,0 +1,17 @@
+names = [ "ken sikes" , "John" , "Jimmy", "Laura", "Danny", "Rob",]
+
+
+print(f"Hello,",names[0].title(),", good to see you again.")
+print(f"Hello,",names[1].title(),", good to see you again.")
+print(f"Hello,",names[2].title(),", good to see you again.")
+print(f"Hello,",names[3].title(),", good to see you again.")
+print(f"Hello,",names[4].title(),", good to see you again.")
+print(f"Hello,",names[5].title(),", good to see you again.")
+
+transportation = ["motorcycle" , "bus" , "car" , "plane", "boat]"]
+
+print(f"I love riding my" , transportation[0],"on a beautiful day")
+print(f"Riding the" , transportation[1], "in San Diego can be rewarding")
+print(f"When it's raining, it's helpful to ride with my wife in her", transportation[2])
+print(f"I used to fly on the", transportation[3], "a lot for my job")
+print(f"By far, my favorite mode of transport though is sailing on a", transportation[4])
