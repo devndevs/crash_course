906ae06827e9f34f19d234fd1307ad4e471e386c
dperkheezy <dperkins1122@gmail.com>
2020-09-11 16:13:14 -0700

    Chapter 3 Complete

---
+++ Chapter3/Chapter3-3.py
@@ -0,0 +1,33 @@
+visit = ["new zealand" , "tahiti" , "japan" , "singapore" , "Alaska"]
+print(visit)
+print()
+print(sorted(visit))
+print()
+print(visit)
+print()
+visit.reverse()
+print()
+print(visit)
+visit.reverse()
+print()
+print(visit)
+visit.sort()
+print()
+print(visit)
+visit.sort(reverse=True)
+
+print()
+guest_list= ["Marcus Aurelius" , "Martin Luther King Jr." , "George Carlin"]
+
+guest_list.insert(0 ,"Rosa Parks")
+guest_list.insert(2, "Robin Williams")
+guest_list.append("Abraham Lincoln")
+
+print("There will be " +str(len(guest_list)) +" guests joining us for dinner.")
+
+hikes = ["grand canyon" , "moab" , "mt. white" , "pct"]
+
+print(hikes)
+
+
+
