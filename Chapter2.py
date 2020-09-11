90021040c1458aa956e984f8b2f2b97869a4d7b8
dperkheezy <dperkins1122@gmail.com>
2020-09-10 17:38:49 -0700

    Chapter Two Complete

---
+++ Chapter2.py
@@ -0,0 +1,12 @@
+print("hello world")
+message = "this works"
+print(message.upper())
+x, y, z = 1,5,10
+print(x + y)
+
+print("My Favorite number is" , x + z)
+#this is just me fooling around  with the code.
+#I may have found my happy place with this editor
+import this
+
+
