aac7a9ab45d0313de8398465e1d3a86d4a965af2
dperkheezy <dperkins1122@gmail.com>
2020-09-11 22:05:19 -0700

    First exorcise with some updates on past ones with new skills

---
+++ Chapter 4/4-1.py
@@ -0,0 +1,10 @@
+pizza = [ "buffalo Chicken" , "sausage" , "hawaiian"]
+for za in pizza:
+    print(f"I really love" , (za.title()), "pizza.\n")
+print("I Really love pizza.\n")
+
+animals = [ "dog" , "cat", "raccoon"]
+for pets in animals:
+    print(f"A",pets.title() ,"would make a great pet.\n")
+    print(f"What a" , pets.title() , "has in common with the other pets is it's furry.\n")
+print(f"All of these animals would make great pets")
--- Chapter3/Chapter3-3.py
+++ Chapter3/Chapter3-3.py
@@ -1,33 +1,32 @@
-visit = ["new zealand" , "tahiti" , "japan" , "singapore" , "Alaska"]
-print(visit)
-print()
-print(sorted(visit))
-print()
-print(visit)
-print()
+visit = ["new zealand" , "tahiti" , "japan" , "singapore" , "alaska"]
+print(visit, "\n")
+
+print(sorted(visit), "\n")
+
+print(visit, "\n")
+
 visit.reverse()
-print()
-print(visit)
+
+print(visit, "\n")
 visit.reverse()
-print()
-print(visit)
+
+print(visit, "\n")
 visit.sort()
-print()
-print(visit)
+
+print(visit, "\n")
 visit.sort(reverse=True)
 
-print()
+
 guest_list= ["Marcus Aurelius" , "Martin Luther King Jr." , "George Carlin"]
 
 guest_list.insert(0 ,"Rosa Parks")
 guest_list.insert(2, "Robin Williams")
 guest_list.append("Abraham Lincoln")
 
-print("There will be " +str(len(guest_list)) +" guests joining us for dinner.")
+print("There will be " +str(len(guest_list)) +" guests joining us for dinner.\n")
 
 hikes = ["grand canyon" , "moab" , "mt. white" , "pct"]
 
 print(hikes)
-
 
 
