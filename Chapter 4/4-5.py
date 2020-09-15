88088ed3bc77afcb48577312c13c2517226ec7fc
dperkheezy <dperkins1122@gmail.com>
2020-09-14 21:52:35 -0700

    Tuple

---
+++ Chapter 4/4-5.py
@@ -0,0 +1,10 @@
+buffet= ("fish",  "pasta" , "soup" , "salad" , "rice")
+for buff in buffet:
+    print(buff)
+
+print()
+#3uffet[0] = "dessert"
+
+buffet= ("chicken" , "pasta" , "sushi" , "salad" , "rice")
+for buff in buffet:
+    print(buff)
