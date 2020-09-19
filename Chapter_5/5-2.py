706529f79b58aa1ed08b5dc62dbe646194edb377
dperkheezy <dperkins1122@gmail.com>
2020-09-18 17:38:40 -0700

    Chapter 5 exorcise 2

---
+++ Chapter_5/5-2.py
@@ -0,0 +1,47 @@
+alien_color = "green"
+if alien_color == "green":
+    print("You earned 5 points")
+   
+if alien_color == "Yellow":
+    print()
+
+alien_color = "yellow"
+if alien_color == "yellow":
+    print("\nYou have just earned 5 points")
+else:
+    print("/n You Have just earned 25 points")
+
+alien_color = "green"
+
+if alien_color == "yellow":
+    print("\nYou have just earned 25 points")
+else:
+    print("\nYou Have just earned 25 points")
+
+age = 31 
+if age < 2:
+    print("\nThis person is a baby")
+elif age >= 2 and age < 4:
+    print("\nThis person is a toddler")
+elif age >= 4 and age < 13:
+    print("\nThis person is a kid")
+elif age >= 13 and age < 20:
+    print("\nThis person is a teenager")
+elif age >= 20 and age < 65:
+    print("\nThis person is an adult")
+else:
+    print("\nThis person is an elder")
+
+favorite_fruits = ["blueberries" , "raspberries" , "strawberries"]
+
+if "bananas" in favorite_fruits:
+    print("\nYou really like bananas!")
+if "blueberries" in favorite_fruits:
+    print("\nYou really like blueberries!")
+if "apples" in favorite_fruits:
+    print("\nYou really like apples!")
+if "raspberries" in favorite_fruits:
+    print("\nYou really like raspberries!")
+if "strawberries" in favorite_fruits:
+    print("\nYou really like strawberries!")
+
