c942eb264b7f2e110676a187bac074db74eb87e1
dperkheezy <dperkins1122@gmail.com>
2020-11-05 22:15:57 -0800

    mass

---
+++ Chapter_8/shirts.py
@@ -0,0 +1,4 @@
+def make_shirt(text,size):
+    print(f"Thank you for ordering a shirt from us in the size {size} and with the message printed on it that reads: ''{text}.'' ")
+
+make_shirt("I think it's turning into fall" , "medium")
