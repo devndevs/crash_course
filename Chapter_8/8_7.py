419804b16f8f6178bbca570fb181afe7785c0b28
dperkheezy <dperkins1122@gmail.com>
2020-11-05 22:26:07 -0800

    Chapter 8 complete

--- Chapter_8/8_7.py
+++ Chapter_8/8_7.py
@@ -1,1 +1,14 @@
 import shirts
+
+from shirts import make_shirt
+print()
+make_shirt("Imported goods", " Large")
+
+from shirts import make_shirt as ms
+print()
+ms("Abriviated Import" , "extra small")
+
+import shirts as sh
+
+from shirts import *
+
